7dc8ac6b6da09003d37ae493c3da3c4e059d90fd
John Hawthorn <john@hawthorn.email>
2021-05-06 22:09:58 -0700

    Add tailwind extras

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -9,5 +9,8 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }

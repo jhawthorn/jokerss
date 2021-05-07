a34b1bc7764efa9fd3624bdb1a8aa6441c64f7a9
John Hawthorn <john@hawthorn.email>
2021-05-07 16:15:52 -0700

    Cleanup title if it contains feed name

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,10 @@
 class Entry < ApplicationRecord
   belongs_to :feed
+
+  def pretty_title
+    title = self.title.dup
+    title.gsub!("#{feed.title} - ", "")
+    title.gsub!("- #{feed.title}", "")
+    title
+  end
 end

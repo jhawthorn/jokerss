4ea42a894ee11ec39fc4754ee2bd5c282eec1a05
John Hawthorn <john@hawthorn.email>
2021-05-09 19:19:29 -0700

    Set sqlite to WAL

---
+++ db/migrate/20210510021713_set_sqlite_to_wal_mode.rb
@@ -0,0 +1,12 @@
+class SetSqliteToWalMode < ActiveRecord::Migration[7.0]
+  def up
+    # https://www.sqlite.org/wal.html
+    # Setting this once will persist the setting in the database file
+    if connection.class.name =~ /SQLite/i
+      connection.execute("PRAGMA journal_mode=WAL")
+    end
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_053209) do
+ActiveRecord::Schema.define(version: 2021_05_10_021713) do
 
   create_table "entries", force: :cascade do |t|
     t.string "title"
